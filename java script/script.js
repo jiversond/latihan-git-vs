9d4d09e82bf002481e6533dd29cf98411da1b3e4
jiversond <141613198+jiversond@users.noreply.github.com>
2023-08-07 14:02:02 +0700

    first commit

---
+++ java script/script.js
@@ -0,0 +1,6 @@
+// Toogle class active
+const navbarnav = document/querySelector ('.navbar-nav');
+// ketika menu diklik
+document.querySelector(menubar).onclick = () => {
+    navbarnav.classList.toggle ('active');
+}
